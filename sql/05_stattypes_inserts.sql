1d18070fc4a11b815035c066c356c468264adbbc
Box263 <kwin6@asu.edu>
2026-04-29 23:40:38 -0700

    flask app with db connection and filters

---
+++ sql/05_stattypes_inserts.sql
@@ -0,0 +1,15 @@
+-- stattypes inserts
+
+INSERT INTO stattypes (stat_id, stat_name) VALUES (1, 'Blast ATK');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (2, 'Blast DEF');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (3, 'Change CD');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (4, 'Critical Rate');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (5, 'Explode Resist');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (6, 'HP');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (7, 'Impact Resist');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (8, 'Ki Recover');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (9, 'Pierce Resist');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (10, 'Slice Resist');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (11, 'Strike ATK');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (12, 'Strike DEF');
+INSERT INTO stattypes (stat_id, stat_name) VALUES (13, 'Vanish Recover');
